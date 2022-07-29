71a8b5744ad0814fe0fa443974866ca6f1dea671
ZhuWei <zzzhu_wei@163.com>
2022-07-29 11:21:23 +0800

    hot-fix commit

--- src/main/java/com/atguigu/test.java
+++ src/main/java/com/atguigu/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4 --hot fix");
     }
 }
