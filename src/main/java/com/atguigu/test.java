04e19576a76a7c6d6a44ebb5345f5320767a5e82
ZhuWei <zzzhu_wei@163.com>
2022-07-29 11:08:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/test.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author ZHUWEI
+ * @Description
+ * @create 2022-07-29-11:05
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
